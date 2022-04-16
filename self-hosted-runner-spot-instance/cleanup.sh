fbec0296c6ffd1142981ad018318b4291bf88ea0
Romar Cablao <romarcablao@gmail.com>
2022-04-16 16:13:48 +0800

    Add auto terminate

--- self-hosted-runner-spot-instance/cleanup.sh
+++ self-hosted-runner-spot-instance/cleanup.sh
@@ -11,3 +11,8 @@ sudo /actions-runner/svc.sh uninstall
 
 # Remove configuration
 ./config.sh remove --token $GH_RUNNER_TOKEN
+
+# Terminate instance
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID --region $REGION
