9ca7a1e8c05a755892f1959d89f52d36bc338b64
Romar Cablao <romarcablao@gmail.com>
2022-04-16 17:07:27 +0800

    Update checker script

--- self-hosted-runner-spot-instance/checker.py
+++ self-hosted-runner-spot-instance/checker.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=os.path.basename(".creds"))
 
 # Setup variables
-HOSTNAME = os.getenv('HOSTNAME')
+GH_HOSTNAME = os.getenv('GH_HOSTNAME')
 GH_ORGANIZATION = os.getenv("GH_ORGANIZATION")
 IDLE_TIME_IN_SEC = os.getenv("GH_IDLE_TIMEOUT")
 IDLE_TIME_IN_SEC_FORMATTED = "{} seconds ago".format(IDLE_TIME_IN_SEC)
-ACTIONS_SERVICE = "actions.runner.{}.spot-runner-{}.service".format(GH_ORGANIZATION, HOSTNAME)
+ACTIONS_SERVICE = "actions.runner.{}.spot-runner-{}.service".format(GH_ORGANIZATION, GH_HOSTNAME)
 
 # Check runner service logs
 RESULT = subprocess.Popen(["journalctl", "-u", ACTIONS_SERVICE, "--since", IDLE_TIME_IN_SEC_FORMATTED, "--no-pager"], stdout=subprocess.PIPE).stdout.read()
