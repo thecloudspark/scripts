04efee5aa8ca479af5a9ffd06a621394a216986b
Romar Cablao <romarcablao@gmail.com>
2022-04-16 14:13:59 +0800

    Add checker script

---
+++ self-hosted-runner-spot-instance/checker.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+from dotenv import load_dotenv
+
+# Load env vars
+load_dotenv(dotenv_path=os.path.basename(".creds"))
+
+# Setup variables
+HOSTNAME = os.getenv('HOSTNAME')
+GH_ORGANIZATION = os.getenv("GH_ORGANIZATION")
+IDLE_TIME_IN_SEC = os.getenv("GH_IDLE_TIMEOUT")
+IDLE_TIME_IN_SEC_FORMATTED = "{} seconds ago".format(IDLE_TIME_IN_SEC)
+ACTIONS_SERVICE = "actions.runner.{}.spot-runner-{}.service".format(GH_ORGANIZATION, HOSTNAME)
+
+# Check runner service logs
+RESULT = subprocess.Popen(["journalctl", "-u", ACTIONS_SERVICE, "--since", IDLE_TIME_IN_SEC_FORMATTED, "--no-pager"], stdout=subprocess.PIPE).stdout.read()
+
+if "No entries" in RESULT: 
+    print("No entries. Triggering cleanup...")
+    CLEANUP_RESULT = subprocess.Popen(["./actions-runner/cleanup.sh"], stdout=subprocess.PIPE).stdout.read()
+    print(CLEANUP_RESULT)
+else:
+    print("With entries")
+    print(RESULT)
